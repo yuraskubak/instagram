0885e373292cf66c96499d263e8521d1797c7848
yuraskubak <yuraskubak1@gmail.com>
2019-10-22 12:43:28 +0300

    Delete changes

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,14 +2,3 @@ class LoginPage():
 
     def __init__(self, driver):
         self.driver = driver
-
-    def enter_username(self, username):
-        self.driver.find_element_by_name("username").clear()
-        self.driver.find_element_by_name("username").send_keys(username)
-
-    def enter_password(self, password):
-        self.driver.find_element_by_name("password").clear()
-        self.driver.find_element_by_name("password").send_keys(password)
-
-    def login(self):
-        self.driver.find_element_by_xpath("//button[@type='submit']").click
